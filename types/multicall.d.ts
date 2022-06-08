783ed6bfb457403dcab0d4b4907c1374056eab78
Mark Milner <mark.milner@avalabs.org>
2022-06-08 16:25:59 -0700

    Fix types

--- types/multicall.d.ts
+++ types/multicall.d.ts
@@ -1,46 +1,46 @@
-declare module '@avalabs/multicall' {
-  import { BigNumber } from 'bignumber.js';
+/// <reference types="node"/>
 
-  export interface IConfig {
-    preset: 'mainnet' | 'kovan' | 'rinkeby' | 'goerli' | 'xdai' | 'ropsten';
-    rpcUrl: string;
-    multicallAddress: string;
-    interval: number;
-    staleBlockRetryWait: number;
-    errorRetryWait: number;
-  }
+import { BigNumber } from 'bignumber.js';
 
-  export interface IPostProcess {
-    (v: any): any;
-  }
-
-  export interface ICall {
-    target: string;
-    call: string[];
-    returns: (string | IPostProcess)[][];
-  }
-
-  export interface IArgumentsMapping {
-    [key: string]: string[];
-  }
-
-  export interface IKeysValues {
-    [key: string]: any;
-  }
-
-  export interface IResult {
-    blockNumber: BigNumber;
-    original: IKeysValues;
-    transformed: IKeysValues;
-    keyToArgMap: IArgumentsMapping;
-  }
-
-  export interface IResponse {
-    results: IResult;
-  }
-
-  export function aggregate(
-    calls: Partial<ICall>[],
-    config: Partial<IConfig>,
-  ): Promise<IResponse>;
+export interface IConfig {
+  preset: 'mainnet' | 'kovan' | 'rinkeby' | 'goerli' | 'xdai' | 'ropsten';
+  rpcUrl: string;
+  multicallAddress: string;
+  interval: number;
+  staleBlockRetryWait: number;
+  errorRetryWait: number;
 }
+
+export interface IPostProcess {
+  (v: any): any;
+}
+
+export interface ICall {
+  target: string;
+  call: string[];
+  returns: (string | IPostProcess)[][];
+}
+
+export interface IArgumentsMapping {
+  [key: string]: string[];
+}
+
+export interface IKeysValues {
+  [key: string]: any;
+}
+
+export interface IResult {
+  blockNumber: BigNumber;
+  original: IKeysValues;
+  transformed: IKeysValues;
+  keyToArgMap: IArgumentsMapping;
+}
+
+export interface IResponse {
+  results: IResult;
+}
+
+export function aggregate(
+  calls: Partial<ICall>[],
+  config: Partial<IConfig>,
+): Promise<IResponse>;
